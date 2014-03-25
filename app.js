17f31ffd64c7ed7addca93bd6a31a0c740313126
Skyler Ng <SkylerNg21@gmail.com>
2014-03-25 13:25:56 -0400

    Added a simple index route.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
 
+app.get('/', require('./routes/routes'));
+
 app.listen(8080);
 console.log('Server is up.');
---
+++ routes/routes.js
@@ -0,0 +1,3 @@
+module.exports = function(request, respond) {
+    respond.send('hello');
+};
