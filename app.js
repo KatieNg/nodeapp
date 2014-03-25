b6844d15dd41d33d56b37f6a88c0825c9dcb8727
Skyler Ng <SkylerNg21@gmail.com>
2014-03-25 13:28:18 -0400

    Renamed the route.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', require('./routes/routes'));
+app.get('/', require('./routes/index'));
 
 app.listen(8080);
 console.log('Server is up.');
